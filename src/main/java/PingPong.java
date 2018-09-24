7f88176dbc156af13b5aebc0f7ae69eb35352eb3
Nefedov Andry <Nefedov_Andry@bk.ru>
2018-09-25 00:42:43 +0300

    Update PingPong.java

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class PingPong {
     public static void main(String[] args) {
+        PingPongComp comp = new PingPongComp();
+
         Scanner scan = new Scanner(System.in);
         String boo;
-        boo = scan.nextLine();
-        boolean bang = boo != null;
-
-
+        while (comp.t.isAlive()) {
+            boo = scan.nextLine();
+            if (boo != null && comp.t.isAlive()) {
+                System.out.println("BANG!");
+                comp.setShoot(true);
+            }
+        }
+        System.out.println("GAME OVER");
     }
 }
